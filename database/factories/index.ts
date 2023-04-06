4458c3fdd2d2569d322f56e1857a5457a84b35fc
bitfella <hello@bitfella.net>
2023-04-06 10:34:35 +0200

    feat: add fake data seeding capabilities for Post entity

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,9 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import Post from 'App/Models/Post'
+
+export const PostFactory = Factory.define(Post, ({ faker }) => {
+  return {
+    title: faker.lorem.words(6),
+    content: faker.lorem.lines(2),
+  }
+}).build()
