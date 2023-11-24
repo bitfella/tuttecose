e910d2cedede6b79bf9cee3b83a1a2e09ffc4d12
bitfella <hello@bitfella.net>
2023-11-24 12:59:10 +0100

    improvement(frontend): add some proper styling

--- frontend/src/components/Todo/Todo.tsx
+++ frontend/src/components/Todo/Todo.tsx
@@ -37,16 +37,65 @@ const Todo = ({ id, title, status }: ITodo): JSX.Element | null => {
   }
 
   return (
-    <li className="mb-4">
+    <li className='mb-4'>
       <input
-        type="checkbox"
+        type='checkbox'
+        className={`
+          appearance-none
+          [&:checked+label]:text-gray-500
+          [&:checked+label]:line-through
+          [&:checked+label]:after:opacity-100
+        `}
         checked={isDone}
         id={todoId}
         name={todoId}
         onChange={handleOptimisticIsDoneChange}
       />
-      <label htmlFor={todoId}>{title}</label>
-      <button onClick={handleOptimisticDelete}>delete</button>
+      <label
+        className={`
+          relative
+          pt-1
+          pl-12
+          cursor-pointer
+          font-bold
+          text-lg
+          before:absolute
+          before:top-0
+          before:left-0
+          before:content
+          before:w-8
+          before:h-8
+          before:rounded-full
+          before:border-[3px]
+          before:border-black
+          before:font-bold
+          before:text-2xl
+          before:text-center
+          after:absolute
+          after:top-[10px]
+          after:left-2
+          after:content
+          after:w-[15px]
+          after:h-[8px]
+          after:border-black
+          after:border-b-[3px]
+          after:border-l-[3px]
+          after:-rotate-45
+          after:opacity-0
+          after:transition-opacity
+          after:ease-in-out
+        `}
+        htmlFor={todoId}
+      >
+        {title}
+      </label>
+      <button
+        className='ml-2'
+        title='delete todo'
+        onClick={handleOptimisticDelete}
+      >
+        [x]
+      </button>
     </li>
   );
 };
